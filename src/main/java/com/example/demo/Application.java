af69090517dc835e89bdd9b98b7fef234dd1a736
Papa Hamed NIANG <paniang@accengage.com>
2018-02-22 11:57:37 +0100

    WIP

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -5,8 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+//
+//    @Bean
+//    public ConfigurableServletWebServerFactory webServerFactory() {
+//        JettyServletWebServerFactory factory = new JettyServletWebServerFactory();
+//        factory.setThreadPool(new QueuedThreadPool(512, 215, 60000, new ArrayBlockingQueue<>(10000)));
+//        return factory;
+//    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
 }
